52ec049754cf8ad89eadecc4a6812d6c838bfbaa
Jouni Kaitasalo <117358885+jkaitasalo@users.noreply.github.com>
2024-01-30 15:33:01 +0200

    Add files via upload

---
+++ src/main/java/bgx151/harj2/Harj2Application.java
@@ -0,0 +1,13 @@
+package bgx151.harj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Harj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Harj2Application.class, args);
+	}
+
+}
---
+++ src/test/java/bgx151/harj2/Harj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package bgx151.harj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Harj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
